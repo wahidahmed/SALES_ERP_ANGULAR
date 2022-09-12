18e2ffe43e7623d1ac8b10a0c17adacd97118c42
wahid-ahmed <wahidahmed1992@gmail.com>
2022-09-12 13:02:18 +0600

    tt

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from './components/navbar/top-navbar/top-navbar.component';
 import { AddProductComponent } from './components/product/add-product/add-product.component';
 
 
+const appRoutes:Routes=[
+  {path:'add_new_product',component:AddProductComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +17,9 @@ import { AddProductComponent } from './components/product/add-product/add-produc
     TopNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
