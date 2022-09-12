29c1ec53862857cb32ecea1b4d07c45c9a066ffe
wahid-ahmed <wahidahmed1992@gmail.com>
2022-09-12 11:21:19 +0600

    set navbar dropdown

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './components/navbar/top-navbar/top-navbar.component';
 import { AddProductComponent } from './components/product/add-product/add-product.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule
