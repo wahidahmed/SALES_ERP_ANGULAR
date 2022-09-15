a90532098f53b92273f2bae785efaf3b375e1214
wahid-ahmed <wahidahmed1992@gmail.com>
2022-09-15 17:29:15 +0600

    add pipe

--- frontend/src/app/services/prodcut.service.ts
+++ frontend/src/app/services/prodcut.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { IProduct } from '../interfaces/IProduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdcutService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  url='/data/product.json';
+
+ //this is the link of pipe function details  https://www.tektutorialshub.com/angular/angular-observable-pipe/
+
+ getProductList():Observable<IProduct[]>{
+  return this.http.get<IProduct[]>(this.url).pipe(
+     map((data) => {
+       return data;
+     }),
+     catchError(this.errorHandler)
+   );
+ }
+
+ errorHandler(error: HttpErrorResponse) {
+  return throwError(error.message || "server error.");
+}
+
 }
