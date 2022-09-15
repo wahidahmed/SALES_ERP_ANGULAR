8ca9418438e2e4b9d910e87565c6a2fc3f951394
wahid-ahmed <wahidahmed1992@gmail.com>
2022-09-15 17:35:58 +0600

    indicate prodcut service in prduct list

--- frontend/src/app/components/product/list-product/list-product.component.ts
+++ frontend/src/app/components/product/list-product/list-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/IProduct';
+import { ProdcutService } from 'src/app/services/prodcut.service';
 
 @Component({
   selector: 'app-list-product',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private prodService:ProdcutService) { }
+productList:Array<IProduct>;
   ngOnInit(): void {
+    this.prodService.getProductList().subscribe(data=>{
+      this.productList=data;
+    });
   }
 
 }
