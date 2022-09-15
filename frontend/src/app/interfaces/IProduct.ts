000e7f46e59ab2e31c8fd075295eee76a900fa5e
wahid-ahmed <wahidahmed1992@gmail.com>
2022-09-15 15:13:09 +0600

    IProduct

---
+++ frontend/src/app/interfaces/IProduct.ts
@@ -0,0 +1,11 @@
+export interface IProduct{
+  Id:number,
+  ProdcutCode:string,
+  ProdcutName:string,
+  ModelId?:number,
+  CostPrice:number,
+  SalesPrice:number,
+  VAT?:number,
+  CatagoryId?:number,
+  UnitId:number
+}
