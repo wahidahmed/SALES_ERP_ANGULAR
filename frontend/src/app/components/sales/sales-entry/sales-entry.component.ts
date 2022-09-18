d6a6edc11b5232f7d6e81455c63ec1d8cb62be3a
wahid-ahmed <wahidahmed1992@gmail.com>
2022-09-18 17:54:46 +0600

    calculate table cell value

--- frontend/src/app/components/sales/sales-entry/sales-entry.component.ts
+++ frontend/src/app/components/sales/sales-entry/sales-entry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/IProduct';
+import { ProdcutService } from 'src/app/services/prodcut.service';
 
 @Component({
   selector: 'app-sales-entry',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-entry.component.css']
 })
 export class SalesEntryComponent implements OnInit {
+[x: string]: any;
 
-  constructor() { }
+  constructor(private productService:ProdcutService) { }
 
+  productList:Array<IProduct>
+  tableRowArray:Array<any>=[];
+  private newAttribute: any =   {
+      qty:null
+      ,salePrice: null
+      ,discount: null
+      ,total:null
+  }
   ngOnInit(): void {
+    this.productService.getProductList().subscribe
+    (
+      (data)=>
+      {
+        this.productList= data
+      }
+    )
+
+   this.addRow();
+
   }
 
+  addRow() {
+   this.newAttribute =   {
+      qty:null
+      ,salePrice: null
+      ,discount: null
+      ,total:null
+  }
+    this.tableRowArray.push(this.newAttribute);
+
+  }
+  deleteRow(index: number) {
+    this.tableRowArray.splice(index, 1);
+  }
+
+  getGrandTotal(){
+
+  //   return marks.reduce((acc:number,{qty,salePrice,discount}: any)=>{
+  //     acc= (Number(qty)*Number(salePrice))-Number(discount);
+  //     return acc;
+  //  },0)
+    return this.tableRowArray.reduce((acc: number, {total}: any) => acc += +(total || 0),0);
+
+  //  this.tableRowArray.reduce((acc,{salePrice,discount,total})=>{
+  //   console.log('acc',acc)
+  //   console.log('salePrice',salePrice)
+  //   console.log('discount',discount)
+  //   console.log('total',total)
+  // },0)
+  }
+
+
 }
+
+
+
+
