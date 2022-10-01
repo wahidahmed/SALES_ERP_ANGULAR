3d1deaa04dc0043c0bdcc87a6483679ca1f6fb58
wahidahmed <wahidahmed1992@gmail.com>
2022-10-01 07:44:50 +0600

    going

--- frontend/src/app/customDirective/filter.directive.ts
+++ frontend/src/app/customDirective/filter.directive.ts
@@ -13,13 +13,25 @@ export class FilterDirective {
   filterData(){
     const filter = new Filter();
     const elem = this.targetElement.nativeElement;
-    const dataList = elem.getAttribute("data-List");
 
-    const filterBy = elem.getAttribute("filter-by");
+    // const filterBy = elem.getAttribute("filter-by");
+    let filterBy = elem.value;
 
-    const filterHead = elem.getAttribute("filter-head");
+    let filterHead = elem.getAttribute("filter-head");
 
-    this.appFilter.filter(filter.doFilter(dataList,filterBy,filterHead));
+  //  let test= this.appFilter.filter(filter.doFilter(dataList,filterBy,filterHead));
+
+  filterBy = filterBy.toLocaleLowerCase();
+
+return this.appFilter.filter((data)=>{
+      // let filterKeys =filterHead.length==0? Object.keys(data): filterHead;
+      let filterKeys =Object.keys(data);
+      return filterKeys.some((keys,i,arr)=>{
+          console.log(data[keys])
+          return String(data[keys]).toLocaleLowerCase().indexOf(filterBy)!==-1;
+        })
+    })
+
   }
 
 }
