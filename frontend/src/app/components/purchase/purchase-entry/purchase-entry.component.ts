19fa27795539dbcf20adb4520742a8ae26327e36
wahid-ahmed <wahidahmed1992@gmail.com>
2022-10-04 17:15:36 +0600

    testing

--- frontend/src/app/components/purchase/purchase-entry/purchase-entry.component.ts
+++ frontend/src/app/components/purchase/purchase-entry/purchase-entry.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { IProduct } from 'src/app/interfaces/IProduct';
 import { ProdcutService } from 'src/app/services/prodcut.service';
@@ -11,7 +11,8 @@ import { ProdcutService } from 'src/app/services/prodcut.service';
 })
 export class PurchaseEntryComponent implements OnInit {
 
-  constructor(private productService:ProdcutService) { }
+
+  constructor(private productService:ProdcutService,private fb:FormBuilder) { }
 
   tableRowList=[
   ];
@@ -27,6 +28,8 @@ export class PurchaseEntryComponent implements OnInit {
 
 productList:IProduct[];
 saveForm:FormGroup;
+public addresses: FormArray;
+public addressForm: FormGroup;//https://prodevhub.com/add-remove-multiple-input-fields-dynamically-using-form-array-in-reactive-forms-angular/
 
    ngOnInit() {
        this.getProductList();
@@ -35,6 +38,36 @@ saveForm:FormGroup;
 
       })
 
+      this.addressForm = this.fb.group({
+        addresses: this.fb.array([ this.createAddress() ])
+     });
+
+  }
+
+  get addressControls() {
+    return this.addressForm.get('addresses')['controls'];
+  }
+
+  createAddress(): FormGroup {
+    return this.fb.group({
+      address: '',
+      street: '',
+      city: '',
+      country: ''
+    });
+  }
+
+  addAddress(): void {
+    this.addresses = this.addressForm.get('addresses') as FormArray;
+    this.addresses.push(this.createAddress());
+  }
+
+  removeAddress(i: number) {
+    this.addresses.removeAt(i);
+  }
+
+  logValue() {
+    console.log('test',this.addresses);
   }
 
    getProductList(){
