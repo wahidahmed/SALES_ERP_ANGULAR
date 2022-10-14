590f0b2f0ff2eb512885f801431fdf1e53970f9e
wahidahmed <wahidahmed1992@gmail.com>
2022-10-14 19:41:45 +0600

    edit data binding in purchase edit

--- frontend/src/app/components/purchase/purchase-edit/purchase-edit.component.ts
+++ frontend/src/app/components/purchase/purchase-edit/purchase-edit.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ISupplier } from 'src/app/interfaces/ISupplier';
 import { Purchase } from 'src/app/Models/purchase';
 import { PurchaseService } from 'src/app/services/purchase.service';
+import { SupplierService } from 'src/app/services/supplier.service';
 
 @Component({
   selector: 'app-purchase-edit',
@@ -10,15 +13,40 @@ import { PurchaseService } from 'src/app/services/purchase.service';
 })
 export class PurchaseEditComponent implements OnInit {
 
-  constructor(private purchaseService:PurchaseService,private acRoute:ActivatedRoute) { }
+  constructor(private purchaseService:PurchaseService,private supplierService:SupplierService,private acRoute:ActivatedRoute,private fb:FormBuilder) { }
+  supplierList:ISupplier[];
 
-  public purchse=new Purchase();
+  editForm:FormGroup;
   ngOnInit() {
-   const id= +this.acRoute.snapshot.paramMap.get('id');
-   this.purchaseService.getPurchaseById(id).subscribe((data:Purchase)=>{
-    this.purchse=data;
-    console.log(this.purchse)
+
+    this.editForm=this.fb.group({
+      SupplierId:['',[Validators.required]],
+      PurchaseDate:['',[Validators.required]],
+      itemList:this.fb.array([]),
+      grandTotal:[null,[]]
    })
+
+    const id= +this.acRoute.snapshot.paramMap.get('id');
+    this.purchaseService.getPurchaseById(id).subscribe((data:Purchase)=>{
+      this.editForm.patchValue(
+        {
+          SupplierId:data.SupplierId,
+
+        }
+      )
+      console.log(this.editForm);
+    })
+    this.getSupplierList();
+
+
+  }
+  getSupplierList(){
+    this.supplierService.getSupplierList().subscribe(data=>{
+        this.supplierList=data;
+      })
   }
 
+  onSubmit(){
+    console.log(this.editForm);
+  }
 }
