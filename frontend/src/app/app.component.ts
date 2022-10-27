f51168a276446d7f8459fcfb4db994b3415bea0b
wahid-ahmed <wahidahmed1992@gmail.com>
2022-10-27 17:20:00 +0600

    aaa

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent {
 
   ngOnInit() {
 
-    // var s = document.createElement("script");
-    // s.type = "text/javascript";
-    // s.src = "../assets/js/main.js";
-    // this.elementRef.nativeElement.appendChild(s);
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "../assets/js/main.js";
+    this.elementRef.nativeElement.appendChild(s);
   }
 
 }
