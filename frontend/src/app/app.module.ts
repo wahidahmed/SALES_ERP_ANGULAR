4f0a869229b96b7fac285fc1b9126e063ec8a240
ahidahmed <wahidahmed192@gmail.com>
2022-10-28 09:50:57 +0600

    remove navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule,Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from'@angular/common/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
-import { TopNavbarComponent } from './components/navbar/top-navbar/top-navbar.component';
 import { AddProductComponent } from './components/product/add-product/add-product.component';
 import { AddNewCustomerComponent } from './components/customer/addNewCustomer/addNewCustomer.component';
 import { CustomerService } from './services/customer.service';
@@ -46,7 +44,6 @@ import { AppRoutingModule } from './custom-module/app-routing/app-routing.module
   declarations: [
     AppComponent
     ,AddProductComponent
-    ,TopNavbarComponent
     ,AddNewCustomerComponent
     ,ListCustomerComponent
     ,ListProductComponent
