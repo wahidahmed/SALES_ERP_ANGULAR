7efb9fef484ff85e01e7c7311aedf326b968a17f
ahidahmed <wahidahmed192@gmail.com>
2022-10-28 11:22:16 +0600

    add register form html

--- frontend/src/app/components/administation/register/register.component.ts
+++ frontend/src/app/components/administation/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
+registerForm:FormGroup;
   ngOnInit() {
   }
 
