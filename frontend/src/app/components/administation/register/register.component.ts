60e4ea4e0cf155a61b668b90495aa77061daab6b
wahid-ahmed <wahidahmed1992@gmail.com>
2022-10-30 17:05:43 +0600

    aaa

--- frontend/src/app/components/administation/register/register.component.ts
+++ frontend/src/app/components/administation/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidatiors } from 'src/app/helpers/custom.validators';
 
 @Component({
   selector: 'app-register',
@@ -13,18 +14,36 @@ export class RegisterComponent implements OnInit {
 registerForm:FormGroup;
 validation_Messge={
   UserName:[{type:'required',message:'User Name is required'}]
+  ,Email:[{type:'emailDomain',message:'email must be "gmail.com" domain'}]
   ,Password:[{type:'required',message:'Password is required'},{type:'minlength',message:'atleast 3 charachters are required'}]
-  ,ConfirmPassword:[{type:'required',message:'ConfirmPassword is required'}]
+  ,ConfirmPassword:[{type:'required',message:'ConfirmPassword is required'},{type:'misMatch',message:'ConfirmPassword is mismatched'}]
 }
   ngOnInit() {
     this.registerForm=this.fb.group({
       UserName:[null,[Validators.required]],
-      Email:[null,[]],
+      Email:[null,[CustomValidatiors.emailDomain('gmail.com')]],
       PW:this.fb.group({
         Password:[null,[Validators.required,Validators.minLength(3)]],
         ConfirmPassword:[null,[Validators.required]]
       })
     })
+  }
+
+  get userNameControl():FormControl{
+    return this.registerForm.get('UserName') as FormControl;
+  }
+  get emailControl():FormControl{
+    return this.registerForm.get('Email') as FormControl;
+  }
+  get pwControl():FormGroup{
+    return this.registerForm.get('PW') as FormGroup;
+  }
+
+  get passwordControl():FormControl{
+    return this.pwControl.get('Password') as FormControl;
+  }
+  get conPasswordControl():FormControl{
+    return this.pwControl.get('ConfirmPassword') as FormControl;
   }
 
   onSubmit(){
