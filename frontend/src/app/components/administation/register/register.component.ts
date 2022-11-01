63b03da719b0bb4abaa2cf97c761b48354d61735
wahid-ahmed <wahidahmed1992@gmail.com>
2022-11-01 16:32:14 +0600

    cros validation message

--- frontend/src/app/components/administation/register/register.component.ts
+++ frontend/src/app/components/administation/register/register.component.ts
@@ -1,54 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { CustomValidatiors } from 'src/app/helpers/custom.validators';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
-  constructor(private fb:FormBuilder) { }
-
-registerForm:FormGroup;
-validation_Messge={
-  UserName:[{type:'required',message:'User Name is required'}]
-  ,Email:[{type:'emailDomain',message:'email must be "gmail.com" domain'}]
-  ,Password:[{type:'required',message:'Password is required'},{type:'minlength',message:'atleast 3 charachters are required'}]
-  ,ConfirmPassword:[{type:'required',message:'ConfirmPassword is required'},{type:'misMatch',message:'ConfirmPassword is mismatched'}]
-  // ,PasswordGroup:[{type:'misMatch',message:'ConfirmPassword is mismatched'}]
-}
+  registerForm: FormGroup;
+  validation_Messge = {
+    UserName: [{ type: 'required', message: 'User Name is required' }],
+    Email: [
+      { type: 'emailDomain', message: 'email must be "gmail.com" domain' },
+    ],
+    Password: [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'atleast 3 charachters are required' },
+    ],
+    ConfirmPassword: [
+      { type: 'required', message: 'ConfirmPassword is required' },
+    ],
+    PasswordGroup: [
+      { type: 'misMatch', message: 'ConfirmPassword is mismatched' },
+    ],
+  };
   ngOnInit() {
-    this.registerForm=this.fb.group({
-      UserName:[null,[Validators.required]],
-      Email:[null,[CustomValidatiors.emailDomain('gmail.com')]],
-      PasswordGroup:this.fb.group({
-        Password:[null,[Validators.required,Validators.minLength(3)]],
-        ConfirmPassword:[null,[Validators.required]]
-      },{ validator: CustomValidatiors.passwordMatch })
-    })
+    this.registerForm = this.fb.group({
+      UserName: [null, [Validators.required]],
+      Email: [null, [CustomValidatiors.emailDomain('gmail.com')]],
+      PasswordGroup: this.fb.group(
+        {
+          Password: [null, [Validators.required, Validators.minLength(3)]],
+          ConfirmPassword: [null, [Validators.required]],
+        },
+        { validator: CustomValidatiors.passwordMatch }
+      ),
+    });
   }
 
-  get userNameControl():FormControl{
+  get userNameControl(): FormControl {
     return this.registerForm.get('UserName') as FormControl;
   }
-  get emailControl():FormControl{
+  get emailControl(): FormControl {
     return this.registerForm.get('Email') as FormControl;
   }
-  get pwControl():FormGroup{
+  get pwControl(): FormGroup {
     return this.registerForm.get('PasswordGroup') as FormGroup;
   }
 
-  get passwordControl():FormControl{
+  get passwordControl(): FormControl {
     return this.pwControl.get('Password') as FormControl;
   }
-  get conPasswordControl():FormControl{
+  get conPasswordControl(): FormControl {
     return this.pwControl.get('ConfirmPassword') as FormControl;
   }
 
-  onSubmit(){
-      console.log(this.registerForm);
+  onSubmit() {
+    console.log(this.registerForm);
   }
-
 }
