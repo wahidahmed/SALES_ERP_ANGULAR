78109d248604165f2e5ac45c18bfa105c7f8d7df
wahidahmed <wahidahmed1992@gmail.com>
2022-11-16 10:45:09 +0600

    a

--- frontend/src/app/helpers/custom.validators.ts
+++ frontend/src/app/helpers/custom.validators.ts
@@ -6,7 +6,6 @@ export class CustomValidatiors{
           return((control: AbstractControl): { [key: string]: any } | null=> {
 
               if(control.value){
-                console.log('sub',control)
                 const email:string=control.value;
                 const domian=email.substring(email.lastIndexOf('@')+1);
                 if(domian.toLocaleLowerCase()===domainName.toLocaleLowerCase()){
