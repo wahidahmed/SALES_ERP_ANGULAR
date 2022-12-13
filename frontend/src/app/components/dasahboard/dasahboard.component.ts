eda7ff2c93543cd8eed2c7b196aa9e6754f533b0
wahidahmed <wahidahmed1992@gmail.com>
2022-12-13 11:51:57 +0600

    add dashboard component

---
+++ frontend/src/app/components/dasahboard/dasahboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dasahboard',
+  templateUrl: './dasahboard.component.html',
+  styleUrls: ['./dasahboard.component.css']
+})
+export class DasahboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
